7219414aa764414999d869da143294ddd8d5c00d
Payton Schubel <pnschubel@stu.naperville203.org>
2018-09-18 11:39:58 -0500

    COMPLETED RUNWAY PROJECT. All classes involved in runway project are completed.

--- ch15Programs/RunwaySimulator.java
+++ ch15Programs/RunwaySimulator.java
@@ -7,22 +7,17 @@ import java.util.Queue;
 public class RunwaySimulator
 {
    //instance variables
-   ArrayList needRunway; //Variable in which to store the queues of landing planes and taking off planes.
+   Queue<String> takeOff; //queue of planes waiting to take off
+   Queue<String> landing; //queue of planes waiting to land
     
    /**
       Constructor.
    */
    public RunwaySimulator()
    {
-      needRunway = new ArrayList();
-      Queue<String> takeOff = new LinkedList<>();
-      Queue<String> landing = new LinkedList<>();
-      
-      //Puts queues in the array, so that I know if planes need to land or not.
-      //Since landing planes take priority, no planes take off until all planes land
-        //in other words, if the queue @ index = 1 has members, nothing happens with queue @ index = 2
-      needRunway.add(landing);
-      needRunway.add(takeOff);
+      //Creates takeOff and landing queues
+      takeOff = new LinkedList<>();
+      landing = new LinkedList<>();
    }
 
    /**
@@ -32,7 +27,7 @@ public class RunwaySimulator
    public void addTakeOff(String flightSymbol)
    {
        //Adds the new plane to the queue of planes needing to take off
-       needRunway[1].add(flightSymbol);
+       this.takeOff.add(flightSymbol);
    }
 
    /**
@@ -42,7 +37,7 @@ public class RunwaySimulator
    public void addLanding(String flightSymbol)
    {
        //Adds the new plane to the queu of planes needing to land
-       needRunway[0].add(flightSymbol);
+       landing.add(flightSymbol);
    }
 
    /**
@@ -51,12 +46,12 @@ public class RunwaySimulator
    public void handleNextAction()
    {
        //Sees if a plane needs to land. If so, lands plane.
-       if (needRunway[0].size != 0){
-           System.out.println("\nFlight " + needRunway[0].remove() + " is landing now.");
+       if (landing.size() != 0){
+           System.out.println("\nFlight " + landing.remove() + " is landing now.");
         }else{
             //Sees if plane needs to take off. If so, lets plane take off.
-            if (needRunway[1].size != 0){
-                System.out.println("\nFlight " + needRunway[0].remove() + " is taking off now.");
+            if (takeOff.size() != 0){
+                System.out.println("\nFlight " + takeOff.remove() + " is taking off now.");
             }else{
                 //If this runs, nothing is due to happen. Tells user so.
                 System.out.println("\nNo flights in need of runway.");
